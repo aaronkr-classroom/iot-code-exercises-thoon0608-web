cc8f65f557616539ca6f82df29479c3760b715ea
thoon0608-web <thoon0608@gmail.com>
2026-04-13 10:23:16 +0900

    Add files via upload

---
+++ exercises/과제/3_1.py
@@ -0,0 +1,12 @@
+#3_1.py
+sensors = {
+    'dht11': {
+        'temperature': 23,
+        'humidity': 47,
+        'unit': 'celsius' },
+    
+    'bh1750': {
+        'illuminance': 450, # 조도 값
+        'unit': 'lux'
+        }
+}
